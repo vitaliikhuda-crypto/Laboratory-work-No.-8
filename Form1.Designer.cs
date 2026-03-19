93f47295e813a75b6a892ab5cbbf3d5b44699d44
vitaliikhuda-crypto <vitalii.khuda@student.uzhnu.edu.ua>
2026-03-19 19:46:57 +0200

    Refactor Form1.Designer.cs for label initialization

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,16 +1,46 @@
-private System.Windows.Forms.Label label1;
-this.label1 = new System.Windows.Forms.Label();
-this.SuspendLayout();
+namespace Laboratory-work-No.-8
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Label label1;
 
-this.label1.AutoSize = true;
-this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
-this.label1.Location = new System.Drawing.Point(200, 150);
-this.label1.Name = "label1";
-this.label1.Size = new System.Drawing.Size(150, 24);
-this.label1.Text = "Перетягни мене";
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
 
-this.label1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label1_MouseDown);
-this.label1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label1_MouseMove);
-this.label1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.label1_MouseUp);
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
 
-this.Controls.Add(this.label1);
+            // label1
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label1.Location = new System.Drawing.Point(150, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Перетягни мене";
+
+            this.label1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label1_MouseDown);
+            this.label1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label1_MouseMove);
+            this.label1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.label1_MouseUp);
+
+            // Form1
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.ClientSize = new System.Drawing.Size(600, 400);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Лабораторна робота №8";
+
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
